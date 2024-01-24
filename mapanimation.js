4bc8ecc77995aecae11f0d6e18b70179c57ee974
Maria Lee <juggleisreal@gmail.com>
2024-01-24 16:14:32 -0500

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,59 @@
+// This array contains the coordinates for all bus stops between MIT and Harvard
+const busStops = [
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.0958, 42.360698],
+  [-71.099558, 42.362953],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
+];
+
+// add own access token
+mapboxgl.accessToken =
+  'pk.eyJ1IjoidGVzdHVzZXIxMDAwIiwiYSI6ImNraDkzZ2pkMzAzMHoycnBmMXpvZ3UwZnMifQ.jAE4YsPeAJv50VK92NSpOQ';
+
+// Function to initialize the map
+function initializeMap() {
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.104081, 42.365554],
+    zoom: 14,
+  });
+}
+
+// Function to add a marker to the map
+function addMarker(map) {
+  return new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
+}
+
+// counter here represents the index of the current bus stop
+let counter = 0;
+
+// Function to move the marker on the map
+function moveMarker(marker) {
+  // Move the marker on the map every 1000ms.
+  setTimeout(() => {
+    if (counter >= busStops.length) return;
+    marker.setLngLat(busStops[counter]);
+    counter++;
+    moveMarker(marker);
+  }, 1000);
+}
+
+// Initialize the map
+let map = initializeMap();
+
+// Add a marker to the map
+let marker = addMarker(map);
+
+// Move the marker when the button is clicked
+function move() {
+  moveMarker(marker);
+}
